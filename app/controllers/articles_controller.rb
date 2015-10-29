0ad2986b75bff794a81a5035e8bdd6c07d2320a5
Gou Furuya <innocent.zero@gmail.com>
2015-10-30 04:55:11 +0900

    Create up/down vote actions and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :article, only: [:up_vote, :down_vote]
+  around_action :render_back, only: [:up_vote, :down_vote]
+
   def index
     @articles = Article.all
   end
@@ -7,8 +10,22 @@ class ArticlesController < ApplicationController
   end
 
   def up_vote
+    @article.up_vote!
   end
 
   def down_vote
+    @article.up_vote!
+  end
+
+  private
+
+  def render_back
+    respond_to do |format|
+      format.html { redirect_to :back }
+    end
+  end
+
+  def article
+    @article = Article.find(params[:id])
   end
 end
