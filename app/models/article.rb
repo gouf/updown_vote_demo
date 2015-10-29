e601631d5019bd3bc75d06c6a1422b6f794d71d3
Gou Furuya <innocent.zero@gmail.com>
2015-10-30 04:51:50 +0900

    Add up/down vote method in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,24 @@
 class Article < ActiveRecord::Base
+
+  def up_vote!
+    commit_vote(:up)
+  end
+
+  def down_vote!
+    commit_vote(:down)
+  end
+
+  private
+
+  def commit_vote(up_down)
+    Article.transaction do
+      case up_down
+      when :+
+        self.vote += 1
+      when :-
+        self.vote -= 1
+      end
+      save
+    end
+  end
 end
